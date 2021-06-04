a1ff2adbac9650822aa8f9082c1bde1ab257bf21
DmitryPoverov <did.ink@mail.ru>
2021-06-04 15:56:38 +0300

    Optimized "Matrix" program

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -34,73 +34,73 @@ public class Matrix {
         System.out.println();
     }
 
-/*    public int[][] sumMatrices (int[][] matrix2) {
-
-        int[][] temp = new int[1][1];
-
-        if ((matrix.length > matrix2.length) && (matrix[0].length < matrix2[0].length)) {
-            System.out.println("Algorithm 1");
-
-            temp = new int[matrix2.length][matrix[0].length];
-
-            for (int i=0; i<matrix2.length; i++) {
-                for (int j=0; j<matrix[i].length; j++) {
-                    temp[i][j] = matrix[i][j] + matrix2[i][j];
-                }
-            }
-
-        } else if ((matrix.length < matrix2.length) && (matrix[0].length > matrix2[0].length)) {
-            System.out.println("Algorithm 2");
-
-            temp = new int[matrix.length][matrix2[0].length];
-
-            for (int i=0; i<matrix.length; i++) {
-                for (int j=0; j<matrix2[0].length; j++) {
-                    temp[i][j] = matrix[i][j] + matrix2[i][j];
-                }
-            }
-
-        } else if ((matrix.length == matrix2.length) && (matrix[0].length < matrix2[0].length)) {
-            System.out.println("Algorithm 3");
-
-            temp = new int[matrix.length][matrix[0].length];
-
-            for (int i=0; i<matrix.length; i++) {
-                for (int j=0; j<matrix[i].length; j++) {
-                    temp[i][j] = matrix[i][j] + matrix2[i][j];
-                }
-            }
-
-        } else if ((matrix.length == matrix2.length) && (matrix[0].length > matrix2[0].length)) {
-            System.out.println("Algorithm 4");
-
-            temp = new int[matrix.length][matrix2[0].length];
-
-            for (int i=0; i<matrix.length; i++) {
-                for (int j=0; j<matrix2[i].length; j++) {
-                    temp[i][j] = matrix[i][j] + matrix2[i][j];
-                }
-            }
-
-        } else if ((matrix.length == matrix2.length) && (matrix[0].length == matrix2[0].length)) {
-            System.out.println("Algorithm 5");
-
-            temp = new int[matrix.length][matrix[0].length];
-
-            for (int i=0; i<matrix.length; i++) {
-                for (int j=0; j<matrix[i].length; j++) {
-                    temp[i][j] = matrix[i][j] + matrix2[i][j];
-                }
-            }
-        } return temp;
-    }*/
+//    public int[][] sumMatrices (int[][] matrix2) {
+//
+//        int[][] temp = new int[1][1];
+//
+//        if ((matrix.length > matrix2.length) && (matrix[0].length < matrix2[0].length)) {
+//            System.out.println("Algorithm 1");
+//
+//            temp = new int[matrix2.length][matrix[0].length];
+//
+//            for (int i=0; i<matrix2.length; i++) {
+//                for (int j=0; j<matrix[i].length; j++) {
+//                    temp[i][j] = matrix[i][j] + matrix2[i][j];
+//                }
+//            }
+//
+//        } else if ((matrix.length < matrix2.length) && (matrix[0].length > matrix2[0].length)) {
+//            System.out.println("Algorithm 2");
+//
+//            temp = new int[matrix.length][matrix2[0].length];
+//
+//            for (int i=0; i<matrix.length; i++) {
+//                for (int j=0; j<matrix2[0].length; j++) {
+//                    temp[i][j] = matrix[i][j] + matrix2[i][j];
+//                }
+//            }
+//
+//        } else if ((matrix.length == matrix2.length) && (matrix[0].length < matrix2[0].length)) {
+//            System.out.println("Algorithm 3");
+//
+//            temp = new int[matrix.length][matrix[0].length];
+//
+//            for (int i=0; i<matrix.length; i++) {
+//                for (int j=0; j<matrix[i].length; j++) {
+//                    temp[i][j] = matrix[i][j] + matrix2[i][j];
+//                }
+//            }
+//
+//        } else if ((matrix.length == matrix2.length) && (matrix[0].length > matrix2[0].length)) {
+//            System.out.println("Algorithm 4");
+//
+//            temp = new int[matrix.length][matrix2[0].length];
+//
+//            for (int i=0; i<matrix.length; i++) {
+//                for (int j=0; j<matrix2[i].length; j++) {
+//                    temp[i][j] = matrix[i][j] + matrix2[i][j];
+//                }
+//            }
+//
+//        } else if ((matrix.length == matrix2.length) && (matrix[0].length == matrix2[0].length)) {
+//            System.out.println("Algorithm 5");
+//
+//            temp = new int[matrix.length][matrix[0].length];
+//
+//            for (int i=0; i<matrix.length; i++) {
+//                for (int j=0; j<matrix[i].length; j++) {
+//                    temp[i][j] = matrix[i][j] + matrix2[i][j];
+//                }
+//            }
+//        } return temp;
+//    }
 
     public int[][] sumMatrices (int[][] matrix2) {
 
 //        int x = matrix.length<matrix2.length? matrix.length : matrix2.length;
-        int x = Math.min(matrix.length, matrix2.length);
-
 //        int y = matrix[0].length<matrix2[0].length? matrix[0].length : matrix2[0].length;
+
+        int x = Math.min(matrix.length, matrix2.length);
         int y = Math.min(matrix[0].length, matrix2[0].length);
 
         int[][] temp = new int[x][y];
