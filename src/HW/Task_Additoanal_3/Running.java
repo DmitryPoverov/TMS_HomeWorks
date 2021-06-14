49223bf6cf1ffcb1fbdaf55a32ef3cd1241548fe
DmitryPoverov <did.ink@mail.ru>
2021-06-14 13:14:09 +0300

    The 3 additional task was solved

--- src/HW/Task_Additoanal_3/Running.java
+++ src/HW/Task_Additoanal_3/Running.java
@@ -1,12 +1,28 @@
 package HW.Task_Additoanal_3;
 
 public class Running {
-
     public static void main(String[] args) {
 
-        Year season = Year.WINTER;
+        Year season;
 
+        System.out.println("\nHere are all elements of the 'Year' enum:");
+        for (Year s : Year.values()) {
+            System.out.println(s);
+        }
 
+        season = Year.valueOf("WINTER");    // выдаст ошибку, если компилятор в enum не найдет введенную стороку.
 
+        System.out.println("\nHere are a description method of all elements of the 'Year' enum:");
+        for (Year s : Year.values()) {
+            System.out.println(s.seasonDescription());
+        }
+
+        Year.favoriteSeason(season);
+
+        for (int i=0; i<Year.values().length; i++) {
+            System.out.println("\n" + Year.values()[i]);
+            System.out.println("The simple average year temperature: " + Year.values()[i].getTemp());
+            System.out.println("This season is " + Year.values()[i].seasonDescription() + "\n");
+        }
     }
 }
--- src/HW/Task_Additoanal_3/Year.java
+++ src/HW/Task_Additoanal_3/Year.java
@@ -2,14 +2,35 @@ package HW.Task_Additoanal_3;
 
 public enum Year {
 
-        WINTER("snow"),
-        SPRING ("flower"),
-        SUMMER ("grass"),
-        FALL ("leaves");
+        WINTER(-10),
+        SPRING (9),
+        SUMMER (23) {
+                public String seasonDescription () {
+                        return "a warm season";
+                }
+        },
+        AUTUMN(11);
 
-        String name1;
+        private int temp;
 
-        Year (String name1) {
-                this.name1 = name1;
+        Year (int temp) {
+                this.temp = temp;
+        }
+
+        int getTemp() {
+                return temp;
+        }
+
+        public String seasonDescription () {
+                return "a cold season";
+        }
+
+        protected static void favoriteSeason (Year season) {
+                switch (season) {
+                        case WINTER -> System.out.println("\nI love winter\n");
+                        case SPRING -> System.out.println("\nI don't love spring\n");
+                        case SUMMER -> System.out.println("\nI don't love summer\n");
+                        case AUTUMN -> System.out.println("\nI don't love autumn\n");
+                }
         }
 }
