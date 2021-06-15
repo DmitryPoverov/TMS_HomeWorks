de3fe211b9a3b7f31ab4e342c204e432f81d1940
DmitryPoverov <did.ink@mail.ru>
2021-06-15 16:17:48 +0300

    The 1 task was solved of the second method

--- src/HW9/Task1_2/Cutter.java
+++ src/HW9/Task1_2/Cutter.java
@@ -1,4 +1,30 @@
 package HW9.Task1_2;
 
 public class Cutter {
+
+    private String s;
+    private char c1;
+    private char c2;
+
+    public Cutter(String s, char c1, char c2) {
+        this.s = s;
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    public String returnString() {
+        int a = s.indexOf(c1);
+        int b = s.indexOf(c2);
+        if (a!=-1 && b!=-1) {
+            s = s.substring(a, b);
+        } else if (a == -1) {
+            s = "There is no one the " + c1 + " character here";
+        } else if (b == -1) {
+            s = "There is no one the " + c2 + " character here";
+        } else {
+            s = "!!! Something went wrong !!!";
+        }
+        return s;
+    }
+
 }
--- src/HW9/Task1_2/Running.java
+++ src/HW9/Task1_2/Running.java
@@ -1,4 +1,16 @@
 package HW9.Task1_2;
 
 public class Running {
+
+    public static void main(String[] args) {
+
+// 1) Вырезать подстроку из строки начиная с первого вхождения символа (А) до,
+// последнего вхождения сивола(B).
+
+        String s = "iT'S A tEsT STriNG, which was WRITING WITH different cases";
+
+        Cutter newCut = new Cutter(s, 'a', 'd');
+
+        System.out.println(newCut.returnString());
+    }
 }
