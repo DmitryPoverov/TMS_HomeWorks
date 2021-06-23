b6ef52ba648fbb6ef3d0bfc198dbf5cda5e2cb2f
DmitryPoverov <did.ink@mail.ru>
2021-06-23 19:51:03 +0300

    Examples for BufferedReader with Scanner

---
+++ src/CW/Task1/BufferedReader_Scanner.java
@@ -0,0 +1,27 @@
+package CW.Task1;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class BufferedReader_Scanner {
+    public static void main(String[] args) {
+
+        try (Scanner scan = new Scanner(new BufferedReader(new FileReader("TestText1.txt")));
+             BufferedWriter bWriter = new BufferedWriter(new FileWriter("TestText2.txt"))) {
+
+            int i;
+
+            while ((i = scan.nextInt()) != -1) {
+                System.out.println((char) i);
+                bWriter.write(i);
+            }
+
+        } catch (FileNotFoundException exc1) {
+            System.out.println("1 " + exc1);
+        } catch (IOException exc2) {
+            System.out.println("2 " + exc2);
+        }
+    }
+}
