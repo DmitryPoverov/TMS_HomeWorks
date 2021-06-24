53373472997a7584a56b88a3632cc0114e9f04fd
DmitryPoverov <did.ink@mail.ru>
2021-06-24 16:38:50 +0300

    Examples for File class

---
+++ src/CW/BufferedReader_Palindrome/FileCW.java
@@ -0,0 +1,18 @@
+package CW.BufferedReader_Palindrome;
+
+import java.io.*;
+
+public class FileCW {
+
+    public static void main(String[] args) {
+
+        File f = new File(".git");
+        File[] sFiles = f.listFiles();
+
+        if (sFiles != null) {
+            for (File file: sFiles) {
+                System.out.println("-> " + file.getAbsolutePath() + "\t<- " + file.getName());
+            }
+        }
+    }
+}
