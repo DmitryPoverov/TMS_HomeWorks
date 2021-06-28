39337b009bdcaa95b12be453eb3badd296b9307a
DmitryPoverov <did.ink@mail.ru>
2021-06-28 13:09:25 +0300

    In examples for Dog Comparing was supplemented

--- src/HW/HW_Examples/Comparable_Example/Comparable_Examples.java
+++ src/HW/HW_Examples/Comparable_Example/Comparable_Examples.java
@@ -16,7 +16,11 @@ public class Comparable_Examples {
         Dog dog8 = new Dog(9, "Timmy", 7.4);
         Dog dog9 = new Dog(11, "Chika", 9.5);
 
-        DogComparator dComp = new DogComparator();
+        Comparator<Dog> dCompN_A = new DogComparator1().thenComparing(new DogComparator2());
+        Comparator<Dog> dCompA = new DogComparator1();
+        Comparator<Dog> dCompN = new DogComparator2();
+        Comparator<Dog> dCompW = new DogComparator3();
+
 
         System.out.println("\nkennel1---------------------------");
 
@@ -46,9 +50,9 @@ public class Comparable_Examples {
             System.out.println(d);
         }
 
-        System.out.println("\nkennel5---------------------------");
+        System.out.println("\nkennel5-Age------------------------");
 
-        SortedSet<Dog> kennel5 = new TreeSet<>(dComp);
+        SortedSet<Dog> kennel5 = new TreeSet<>(dCompA);
         kennel5.add(dog1);
         kennel5.add(dog2);
         kennel5.add(dog3);
@@ -59,13 +63,64 @@ public class Comparable_Examples {
             System.out.println(d);
         }
 
-        System.out.println("\nkennel1---------------------------");
+        System.out.println("\nkennel1_Name-Age------------------");
         kennel1.add(dog4);
         kennel1.add(dog5);
         kennel1.add(dog6);
-        kennel1.sort(dComp);
+        kennel1.sort(dCompN_A);
         for (Dog d: kennel1) {
             System.out.println(d);
+        }
+
+        System.out.println("\nkennel5_Name-----------------------");
+        kennel5 = new TreeSet<>(dCompN);
+        kennel5.addAll(kennel1);
+        kennel5.addAll(kennel2);
+        for (Dog d: kennel5) {
+            System.out.println(d);
+        }
+
+        System.out.println("\nkennel6----------------------------");
+        Map<Integer, Dog> kennel6 = new HashMap<>();
+        kennel6.put(1000, dog0);
+        kennel6.put(22, dog1);
+        kennel6.put(103, dog2);
+        kennel6.put(5, dog3);
+        kennel6.put(69, dog4);
+        kennel6.put(17, dog5);
+        kennel6.put(800, dog6);
+
+        boolean b = kennel6.containsKey(1);
+        System.out.println(b);
+
+        b = kennel6.containsValue(dog0);
+        System.out.println(b);
+
+        b = kennel6.equals(kennel1);
+        System.out.println(b);
+
+        b = kennel6.isEmpty();
+        System.out.println(b);
+
+        Dog dog10 = kennel6.get(5);
+        System.out.println("dog10: " + dog10);
+
+        dog10 = kennel6.get(10);
+        System.out.println("dog10: " + dog10);
+
+        kennel6.putIfAbsent(5, dog10);
+
+        System.out.println("\nkennel6----------------------------");
+        System.out.println(kennel6.size());
+
+        System.out.println("\nkennel6----------------------------");
+        for(Dog d: kennel6.values()) {
+            System.out.println(d);
+        }
+
+        System.out.println("\nkennel6----------------------------");
+        for(int i: kennel6.keySet()) {
+            System.out.println(i);
         }
     }
 }
